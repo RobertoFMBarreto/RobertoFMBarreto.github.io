2f95fc8d1023168f319fb036ba3384a640b04587
Roberto Barreto <robertofmbarreto@gmail.com>
2022-09-03 20:55:48 +0100

    change header

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,9 +7,6 @@ import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
   return (
     <section id="about">
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
-
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
@@ -18,19 +15,6 @@ const About = () => {
         </div>
 
         <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about__icon" />
-              <h5>Experience</h5>
-              <small>5+ Years Working</small>
-            </article>
-            <article className="about__card">
-              <VscFolderLibrary className="about__icon" />
-              <h5>Projects</h5>
-              <small>80+ Completed</small>
-            </article>
-          </div>
-
           <p>
             I learned programming by teaching my self how to code creating games
             in unity with c# as 16 years old, since then i got excited with
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "./Header.css";
-import HeaderButtons from "./HeaderButtons";
-import ME from "../../assets/me.png";
-import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
@@ -10,18 +7,6 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Roberto Barreto</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-
-        <HeaderButtons />
-
-        <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="ME" />
-        </div>
-
-        <a href="#contact" className="scroll__down">
-          Scroll Down
-        </a>
       </div>
     </header>
   );
